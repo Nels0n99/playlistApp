8f32fea908aece36c756339be0b3971d3c52d783
c7n6 <c7n6@fresnou.org>
2023-04-28 11:35:21 -0700

    added code for authentication of user

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const songController = require('../controllers/songController');
+const userController = require('../controllers/userController');
 
 /* GET home page. */
 router.get('/', songController.viewAll);
@@ -10,4 +11,10 @@ router.get('/delete/:id', songController.deleteSong);
 router.get('/add', songController.renderAddForm);
 router.post('/add', songController.addSong);
 
+router.get('/register', userController.renderRegistration);
+router.post('/register', userController.register);
+
+router.get('/login', userController.renderLogin);
+router.post('/login', userController.authenticate);
+router.get('/logout', userController.logout);
 module.exports = router;
