636bef87e60d61c3bd5d66fa10a979bb2f8d88ab
c7n6 <c7n6@fresnou.org>
2023-05-19 11:23:45 -0700

    made small adjustment to the styling of the song card, controller, and a pug file

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,25 +14,14 @@ module.exports.renderRegistration = async function(req, res){
 };
 
 module.exports.register = async function(req, res){
-    const existingUser = await User.findOne({
-        where: {
-            email: req.body.email
-        }
+    await User.create({
+        email: req.body.email,
+        password: md5(req.body.password),
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        role_id: req.body.role
     });
-    if(existingUser){
-        res.render('user/register', {
-            error: 'User Already Exists'
-        })
-    } else {
-        await User.create({
-            email: req.body.email,
-            password: md5(req.body.password),
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            role_id: req.body.role
-        });
-        res.redirect('/login');
-    }
+    res.redirect('/login');
 }
 
 module.exports.renderLogin = function (req, res) {
